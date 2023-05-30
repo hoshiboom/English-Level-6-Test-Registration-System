1827408168fd0e82e7093636558c6fb72ddcc873
hoshiboom <93516189+hoshiboom@users.noreply.github.com>
2023-05-31 05:24:52 +0800

    前端请求封装

--- ExamSystem/src/main/resources/static/Utils/http.mjs
+++ ExamSystem/src/main/resources/static/Utils/http.mjs
@@ -21,12 +21,15 @@ function requests(obj) {
     var urlencoded = new URLSearchParams();
     // get请求的data参数拼接到url后面，post等其他请求的data参数放到body
     if (obj.method === "GET") {
-        let ec = encodeURIComponent;                       // URL编码
-        let queryParams = Object.keys(obj.data)         // 获取data键名
-            .map(k => `${ec(k)}=${ec(obj.data[k])}`)    // 将键名和值连接起来
-            .join('&');
-        if(queryParams) obj.path += `?${queryParams}`;
-        console.log(obj.path)
+        if(obj.data){
+            let ec = encodeURIComponent;                       // URL编码
+            let queryParams = Object.keys(obj.data)         // 获取data键名
+                .map(k => `${ec(k)}=${ec(obj.data[k])}`)    // 将键名和值连接起来
+                .join('&');
+            if(queryParams) obj.path += `?${queryParams}`;
+            console.log(obj.path)
+        }
+
     } else {
         for(var key in obj.data) {
             urlencoded.append(key,obj.data[key]);
@@ -35,13 +38,17 @@ function requests(obj) {
 
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    myHeaders.append("token", obj.token);
 
     var requestOptions = {
-        method: 'POST',
+        method: method,
         headers: myHeaders,
-        body: urlencoded,
         redirect: 'follow'
     };
+
+    if(method!=="GET" && method!=="HEAD"){
+        requestOptions.body=urlencoded
+    }
 
     console.log(requestOptions);
     // 发送请求并返回 promise 对象，注意 fetch不会拦截400/500等异常请求️，只有网络不通时才会失败
