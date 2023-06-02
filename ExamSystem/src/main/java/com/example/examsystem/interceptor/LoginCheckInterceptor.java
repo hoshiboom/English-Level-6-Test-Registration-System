062f5e97be150c88d6e8fc2d13c4354bc38c97c2
hoshiboom <93516189+hoshiboom@users.noreply.github.com>
2023-06-02 15:24:05 +0800

    useless

--- ExamSystem/src/main/java/com/example/examsystem/interceptor/LoginCheckInterceptor.java
+++ ExamSystem/src/main/java/com/example/examsystem/interceptor/LoginCheckInterceptor.java
@@ -34,76 +34,79 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         //1.获取请求url
         String url = request.getRequestURL().toString();
-        //2.判断url是login则放行
-        if(url.contains("login/admin") || url.contains("login/teacher") || url.contains("login/student")){//有漏洞：被恶意构造?login=...
-            return true;
-        }
+        String tmpmethod=request.getMethod();
+        if(!tmpmethod.equals("OPTIONS")){
+            boolean isSignup = tmpmethod.equals("POST")&&(url.contains("student")||url.contains("teacher")||url.contains("admin"));
+            //2.判断url是login则放行
+            if(url.contains("login/admin") || url.contains("login/teacher") || url.contains("login/student")){//有漏洞：被恶意构造?login=...
+                return true;
+            }
 
-        //<editor-fold desc="调试阶段全部放行">
+            //<editor-fold desc="调试阶段全部放行">
 //        if(true){
 //            return true;
 //        }
-        //</editor-fold>
-        log.info("继续返回执行");
-        //3.获取请求头的令牌
-        String tokenvalue = request.getHeader("token");
-        //4.判断令牌是否为空
-        if(tokenvalue == null){
-            log.info("token为空");
-            Response response1 = new Response(ResponseEnum.Login_Failure);
-            Gson gson = new GsonBuilder().serializeNulls().create();//用于有参数为null时的正确序列化，如果不需要为null初始化则直接new Gson()即可
-            response.setCharacterEncoding("UTF-8");//将输出字符流的编码设置为UTF-8
-            response.getWriter().write(gson.toJson(response1));
-            return false;
-        }
-        //5.判断令牌是否正确
-        try {
-            Token token1 = tokenService.getOne(
-                    new QueryWrapper<Token>().eq("value",tokenvalue)
-            );
-            Integer roleId, id;
-            if(token1 == null){
-                Claims claim = JwtUtils.parseJWT(tokenvalue);
-                roleId = claim.get("roleId",Integer.class);
-                id = claim.get("id",Integer.class);
-            }
-            else{
-                roleId = token1.getRoleId();
-                id = token1.getUserId();
-            }
-            //boolean adminRole = (url.contains("admin")||url.contains("student")||url.contains("teacher")||url.contains())
-            if(roleId == 1){//用户是管理员，有全部权限，直接放行
-                return true;
-            }
-            //用户是教师，对于访问卷子、问题、批改、注册的请求放行
-            else if(roleId == 2 && (url.contains("paper")||url.contains("question")||url.contains("doandcheck")||(url.contains("teacher")&&request.getMethod()=="POST"))){
-                return true;
-            }
-            //用户是学生，对于访问卷子、问题的get请求放行，对于访问doandcheck的post请求也放行，对于学生注册、报名考试的请求放行
-            else if(roleId == 3 && ((url.contains("paper")||url.contains("question"))&&(request.getMethod().equals("GET")))||(url.contains("doandcheck")&&request.getMethod().equals("DELETE"))||(url.contains("student")&&request.getMethod().equals("POST"))||url.contains("signup")){
-                if(url.contains("paperorg")||url.contains("question")){//判断有没有报名，未报名返回false
-                    Signup signup=signupService.getOne(new QueryWrapper<Signup>().eq("student_id",id));
-                    if(signup==null){
-                        return false;
-                    }
-                }
-                return true;
-            }
-            else{
+            //</editor-fold>
+            log.info("继续返回执行");
+            //3.获取请求头的令牌
+            String tokenvalue = request.getHeader("token");
+            //4.判断令牌是否为空
+            if(tokenvalue == null){
+                log.info("token为空");
+                Response response1 = new Response(ResponseEnum.Login_Failure);
+                Gson gson = new GsonBuilder().serializeNulls().create();//用于有参数为null时的正确序列化，如果不需要为null初始化则直接new Gson()即可
+                response.setCharacterEncoding("UTF-8");//将输出字符流的编码设置为UTF-8
+                response.getWriter().write(gson.toJson(response1));
                 return false;
             }
+            //5.判断令牌是否正确
+            try {
+                Token token1 = tokenService.getOne(
+                        new QueryWrapper<Token>().eq("value",tokenvalue)
+                );
+                Integer roleId, id;
+                if(token1 == null){
+                    Claims claim = JwtUtils.parseJWT(tokenvalue);
+                    roleId = claim.get("roleId",Integer.class);
+                    id = claim.get("id",Integer.class);
+                }
+                else{
+                    roleId = token1.getRoleId();
+                    id = token1.getUserId();
+                }
+                //boolean adminRole = (url.contains("admin")||url.contains("student")||url.contains("teacher")||url.contains())
+                if(roleId == 1){//用户是管理员，有全部权限，直接放行
+                    return true;
+                }
+                //用户是教师，对于访问卷子、问题、批改、注册的请求放行
+                else if(roleId == 2 && (url.contains("paper")||url.contains("question")||url.contains("doandcheck")||(url.contains("teacher")&&request.getMethod()=="POST"))){
+                    return true;
+                }
+                //用户是学生，对于访问卷子、问题的get请求放行，对于访问doandcheck的post请求也放行，对于学生注册、报名考试的请求放行
+                else if(roleId == 3 && ((url.contains("paper")||url.contains("question"))&&(request.getMethod().equals("GET")))||(url.contains("doandcheck")&&request.getMethod().equals("DELETE"))||(url.contains("student")&&request.getMethod().equals("POST"))||url.contains("signup")){
+                    if(url.contains("paperorg")||url.contains("question")){//判断有没有报名，未报名返回false
+                        Signup signup=signupService.getOne(new QueryWrapper<Signup>().eq("student_id",id));
+                        if(signup==null){
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+                else{
+                    return false;
+                }
 
-        } catch (Exception e) {
-            log.info("token解析失败，返回未登录错误信息");
-            Response response1 = new Response(ResponseEnum.Login_Failure);
-            Gson gson = new GsonBuilder().serializeNulls().create();
-            response.setCharacterEncoding("UTF-8");
-            response.getWriter().write(gson.toJson(response1));
-            return false;
+            } catch (Exception e) {
+                log.info("token解析失败，返回未登录错误信息");
+                Response response1 = new Response(ResponseEnum.Login_Failure);
+                Gson gson = new GsonBuilder().serializeNulls().create();
+                response.setCharacterEncoding("UTF-8");
+                response.getWriter().write(gson.toJson(response1));
+                return false;
+            }
         }
-
         //6.放行
-        //return true;
+        return true;
 
     }
 
